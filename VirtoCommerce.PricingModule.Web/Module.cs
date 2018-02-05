71a7ef0e70c24a463479e5bf657fe86bb64d3522
Yuliya Malik <yuliya.umbrellagroup@gmail.com>
2018-02-05 13:38:39 +0200

    changed way to get a connection string

--- VirtoCommerce.PricingModule.Web/Module.cs
+++ VirtoCommerce.PricingModule.Web/Module.cs
@@ -25,7 +25,7 @@ namespace VirtoCommerce.PricingModule.Web
     public class Module : ModuleBase, ISupportExportImportModule
     {
         private readonly string _connectionStringName = ConfigurationHelper.GetConnectionStringValue("{{ModuleId}}") ?? ConfigurationHelper.GetConnectionStringValue("VirtoCommerce");
-        private readonly IUnityContainer _container;
+        private readonly IUnityContainer _container; 
 
         public Module(IUnityContainer container)
         {
