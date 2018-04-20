e4b831553233f866bde68182a7414ba45ffdd35b
eMazeika <em@virtoway.com>
2018-04-20 17:36:55 +0300

    test Can_return_prices_from_many_pricelists_by_priority updated re #19

--- VirtoCommerce.PricingModule.Test/PriceScenarios.cs
+++ VirtoCommerce.PricingModule.Test/PriceScenarios.cs
@@ -29,55 +29,48 @@ namespace VirtoCommerce.PricingModule.Tests
         }
 
         [Fact]
-        public void Can_return_prices_by_priority()
+        public void Can_return_prices_from_many_pricelists_by_priority()
         {
             var evalContext = new Domain.Pricing.Model.PriceEvaluationContext
             {
                 ProductIds = new[] { "ProductId" },
-                PricelistIds = new[] { "Pricelist 1", "Pricelist 2" }
+                PricelistIds = new[] { "Pricelist 1", "Pricelist 2", "Pricelist 3" }
             };
 
             var mockPrices = new[] {
-                new PriceEntity { List = 11, MinQuantity = 10, PricelistId = "Pricelist 2" },
-                new PriceEntity { List = 11, MinQuantity = 15, PricelistId = "Pricelist 2" },
-                new PriceEntity { List = 11, MinQuantity = 44, PricelistId = "Pricelist 2" },
-                new PriceEntity { List = 11, MinQuantity = 50, PricelistId = "Pricelist 2" },
-                new PriceEntity { List = 99, MinQuantity = 1,  PricelistId = "Pricelist 2" },
-                new PriceEntity { List = 95, MinQuantity = 5,  PricelistId = "Pricelist 2" },
+                new PriceEntity { List = 10, MinQuantity = 2, PricelistId = "Pricelist 1" },
 
-                new PriceEntity { List = 55, MinQuantity = 10, PricelistId = "Pricelist 1" },
-                new PriceEntity { List = 66, MinQuantity = 20, PricelistId = "Pricelist 1" },
-                new PriceEntity { List = 44, MinQuantity = 44, PricelistId = "Pricelist 1" }
+                new PriceEntity { List = 9, MinQuantity = 1, PricelistId = "Pricelist 2" },
+                new PriceEntity { List = 10, MinQuantity = 2, PricelistId = "Pricelist 2" },
+
+                new PriceEntity { List = 6, MinQuantity = 2, PricelistId = "Pricelist 3" },
+                new PriceEntity { List = 5, MinQuantity = 3,  PricelistId = "Pricelist 3" }                
             };
             for (int i = 0; i < mockPrices.Length; i++)
             {
                 mockPrices[i].Id = i.ToString();
                 mockPrices[i].ProductId = "ProductId";
             }
-
             var pricingService = GetPricingService(() => GetPricingRepositoryMock(mockPrices));
 
             var prices = pricingService.EvaluateProductPrices(evalContext).ToArray();
 
-            Assert.Equal(5, prices.Length);
-            Assert.Equal(mockPrices[4].Id, prices[0].Id);
-            Assert.Equal(mockPrices[5].Id, prices[1].Id);
-            Assert.Equal(mockPrices[6].Id, prices[2].Id);
-            Assert.Equal(mockPrices[7].Id, prices[3].Id);
-            Assert.Equal(mockPrices[8].Id, prices[4].Id);
+            // only 2 prices (from higher priority pricelists) returned, but not for MinQuantity == 3
+            Assert.Equal(2, prices.Length);
+            Assert.Equal(mockPrices[1].Id, prices[0].Id);
+            Assert.Equal(mockPrices[0].Id, prices[1].Id);
+            Assert.DoesNotContain(prices, x => x.MinQuantity == 3);
 
-            // order changed
-            evalContext.PricelistIds = new[] { "Pricelist 2", "Pricelist 1" };
-
+            // Pricelist priority changed
+            evalContext.PricelistIds = new[] { "Pricelist 3", "Pricelist 2", "Pricelist 1" };
             prices = pricingService.EvaluateProductPrices(evalContext).ToArray();
 
-            Assert.Equal(6, prices.Length);
-            Assert.Equal(mockPrices[4].Id, prices[0].Id);
-            Assert.Equal(mockPrices[5].Id, prices[1].Id);
-            Assert.Equal(mockPrices[0].Id, prices[2].Id);
-            Assert.Equal(mockPrices[1].Id, prices[3].Id);
-            Assert.Equal(mockPrices[2].Id, prices[4].Id);
-            Assert.Equal(mockPrices[3].Id, prices[5].Id);
+            // 3 prices returned, but not from "Pricelist 1"
+            Assert.Equal(3, prices.Length);
+            Assert.Equal(mockPrices[1].Id, prices[0].Id);
+            Assert.Equal(mockPrices[3].Id, prices[1].Id);
+            Assert.Equal(mockPrices[4].Id, prices[2].Id);
+            Assert.DoesNotContain(prices, x => x.PricelistId == "Pricelist 1");
         }
 
         private IPricingService GetPricingService(Func<IPricingRepository> repositoryFactory)
