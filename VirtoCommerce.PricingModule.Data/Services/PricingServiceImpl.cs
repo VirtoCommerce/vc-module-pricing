e6c8f645a1b6aedb600a017f0d075de3e7a66038
Wim Vergouwe <wim.vergouwe@delawareconsulting.com>
2018-10-18 07:49:42 +0200

    Simplify FilterTimeOverlappingPrices

--- VirtoCommerce.PricingModule.Data/Services/PricingServiceImpl.cs
+++ VirtoCommerce.PricingModule.Data/Services/PricingServiceImpl.cs
@@ -498,15 +498,12 @@ namespace VirtoCommerce.PricingModule.Data.Services
                 .Where(x => x != null)
                 .GroupBy(x => x.ProductId))
             {
-                var orderedPrices = pricesForProduct
+                yield return pricesForProduct
                     // Startdate closest to certainDate is more important.
                     .OrderBy(x => certainDate.Subtract(x.StartDate.GetValueOrDefault()).TotalSeconds)
                     // Enddate closest to certainDate is more important.
-                    .ThenBy(x => x.EndDate.GetValueOrDefault().Subtract(certainDate).TotalSeconds);
-
-                var filteredPrice = orderedPrices.FirstOrDefault();
-                if (filteredPrice != null)
-                    yield return filteredPrice;
+                    .ThenBy(x => x.EndDate.GetValueOrDefault().Subtract(certainDate).TotalSeconds)
+                    .First();
             }
         }
 
