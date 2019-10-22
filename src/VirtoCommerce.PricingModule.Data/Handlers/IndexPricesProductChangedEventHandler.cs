7309be2f619a8a3de7d2dfb5ac2d70b8956e07af
Andrei Kostyrin <kostyrin2@gmail.com>
2019-10-22 12:28:42 +0200

    #vp-732 IndexingJobs

--- src/VirtoCommerce.PricingModule.Data/Handlers/IndexPricesProductChangedEventHandler.cs
+++ src/VirtoCommerce.PricingModule.Data/Handlers/IndexPricesProductChangedEventHandler.cs
@@ -1,24 +1,16 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
-using Hangfire;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Events;
 using VirtoCommerce.PricingModule.Core.Events;
 using VirtoCommerce.SearchModule.Core.Model;
-using VirtoCommerce.SearchModule.Core.Services;
+using VirtoCommerce.SearchModule.Data.BackgroundJobs;
 
 namespace VirtoCommerce.PricingModule.Data.Handlers
 {
     public class IndexPricesProductChangedEventHandler : IEventHandler<PriceChangedEvent>
     {
-        private readonly IIndexingManager _indexingManager;
-        private static readonly EntryState[] _entityStates = new[] { EntryState.Added, EntryState.Modified, EntryState.Deleted };
-
-        public IndexPricesProductChangedEventHandler(IIndexingManager indexingManager)
-        {
-            _indexingManager = indexingManager;
-        }
 
         public Task Handle(PriceChangedEvent message)
         {
@@ -27,30 +19,13 @@ namespace VirtoCommerce.PricingModule.Data.Handlers
                 throw new ArgumentNullException(nameof(message));
             }
 
-            var indexProductIds = message.ChangedEntries.Where(x => _entityStates.Any(s => s == x.EntryState)
-                                                                    && x.OldEntry.ProductId != null)
-                                                          .Select(x => x.OldEntry.ProductId)
-                                                          .Distinct().ToArray();
+            var indexEntries = message.ChangedEntries
+                .Select(x => new IndexEntry { Id = x.OldEntry.ProductId, EntryState = EntryState.Modified, Type = KnownDocumentTypes.Product })
+                .ToArray();
 
-            if (!indexProductIds.IsNullOrEmpty())
-            {
-                BackgroundJob.Enqueue(() => TryIndexPricesProductBackgroundJob(indexProductIds));
-            }
+            IndexingJobs.EnqueueIndexAndDeleteDocuments(indexEntries);
 
             return Task.CompletedTask;
-        }
-
-
-        [DisableConcurrentExecution(60 * 60 * 24)]
-        public async Task TryIndexPricesProductBackgroundJob(string[] indexProductIds)
-        {
-            await TryIndexPricesProduct(indexProductIds);
-        }
-
-
-        protected virtual async Task TryIndexPricesProduct(string[] indexProductIds)
-        {
-            await _indexingManager.IndexDocumentsAsync(KnownDocumentTypes.Product, indexProductIds);
         }
     }
 }
