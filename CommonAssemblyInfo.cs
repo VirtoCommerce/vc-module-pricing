5004d2eb102253d56655c41c7827c0be85a546a0
Andrey Kozlov <yecli.github@gmail.com>
2020-01-29 17:35:09 +0500

    2.18.11

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Virto Commerce Pricing Module")]
 [assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2019")]
 
-[assembly: AssemblyFileVersion("2.18.10.0")]
-[assembly: AssemblyVersion("2.18.10.0")]
+[assembly: AssemblyFileVersion("2.18.11.0")]
+[assembly: AssemblyVersion("2.18.11.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
