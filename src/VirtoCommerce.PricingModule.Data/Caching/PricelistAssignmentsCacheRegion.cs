497cbf42de0a644a12d07ce2ac5492798c8f4c76
Andrei Kolchanov <akak1977@users.noreply.github.com>
2020-07-29 18:02:41 +0500

    VP-3266: Change PricelistAssignmentsCacheRegion, PricelistsCacheRegion , PricesCacheRegion to support new cache expiration (#96)

--- src/VirtoCommerce.PricingModule.Data/Caching/PricelistAssignmentsCacheRegion.cs
+++ src/VirtoCommerce.PricingModule.Data/Caching/PricelistAssignmentsCacheRegion.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Concurrent;
-using System.Threading;
 using Microsoft.Extensions.Primitives;
 using VirtoCommerce.Platform.Core.Caching;
 
@@ -8,25 +6,18 @@ namespace VirtoCommerce.PricingModule.Data.Caching
 {
     public class PricelistAssignmentsCacheRegion : CancellableCacheRegion<PricelistAssignmentsCacheRegion>
     {
-        private static readonly ConcurrentDictionary<string, CancellationTokenSource> _pricelistAssignmentRegionTokenLookup = new ConcurrentDictionary<string, CancellationTokenSource>();
-
         public static IChangeToken CreateChangeToken(string pricelistAssignmentId)
         {
             if (string.IsNullOrEmpty(pricelistAssignmentId))
             {
                 throw new ArgumentNullException(nameof(pricelistAssignmentId));
             }
-
-            var cancellationTokenSource = _pricelistAssignmentRegionTokenLookup.GetOrAdd(pricelistAssignmentId, new CancellationTokenSource());
-            return new CompositeChangeToken(new[] { CreateChangeToken(), new CancellationChangeToken(cancellationTokenSource.Token) });
+            return CreateChangeTokenForKey(pricelistAssignmentId);
         }
 
         public static void ExpirePricelistAssignment(string pricelistAssignmentId)
         {
-            if (_pricelistAssignmentRegionTokenLookup.TryRemove(pricelistAssignmentId, out var token))
-            {
-                token.Cancel();
-            }
+            ExpireTokenForKey(pricelistAssignmentId);
         }
     }
 }
--- src/VirtoCommerce.PricingModule.Data/Caching/PricesCacheRegion.cs
+++ src/VirtoCommerce.PricingModule.Data/Caching/PricesCacheRegion.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Concurrent;
-using System.Threading;
 using Microsoft.Extensions.Primitives;
 using VirtoCommerce.Platform.Core.Caching;
 
@@ -8,24 +6,18 @@ namespace VirtoCommerce.PricingModule.Data.Caching
 {
     public class PricesCacheRegion : CancellableCacheRegion<PricesCacheRegion>
     {
-        private static readonly ConcurrentDictionary<string, CancellationTokenSource> _priceRegionTokenLookup = new ConcurrentDictionary<string, CancellationTokenSource>();
-
         public static IChangeToken CreateChangeToken(string priceId)
         {
             if (string.IsNullOrEmpty(priceId))
             {
                 throw new ArgumentNullException(nameof(priceId));
             }
-            var cancellationTokenSource = _priceRegionTokenLookup.GetOrAdd(priceId, new CancellationTokenSource());
-            return new CompositeChangeToken(new[] { CreateChangeToken(), new CancellationChangeToken(cancellationTokenSource.Token) });
+            return CreateChangeTokenForKey(priceId);
         }
 
         public static void ExpirePrice(string priceId)
         {
-            if (_priceRegionTokenLookup.TryRemove(priceId, out var token))
-            {
-                token.Cancel();
-            }
+            ExpireTokenForKey(priceId);
         }
     }
 }
