1410fcf518c82689790c45bfeffdb122812d834a
Andrei Kolchanov <akak1977@gmail.com>
2020-07-29 11:16:33 +0500

    VP-3266: Change PricelistAssignmentsCacheRegion, Price, PricesCacheRegion to support new cache expiration

--- src/VirtoCommerce.PricingModule.Data/Caching/PricelistsCacheRegion.cs
+++ src/VirtoCommerce.PricingModule.Data/Caching/PricelistsCacheRegion.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Concurrent;
-using System.Threading;
 using Microsoft.Extensions.Primitives;
 using VirtoCommerce.Platform.Core.Caching;
 
@@ -8,24 +6,18 @@ namespace VirtoCommerce.PricingModule.Data.Caching
 {
     public class PricelistsCacheRegion : CancellableCacheRegion<PricelistsCacheRegion>
     {
-        private static readonly ConcurrentDictionary<string, CancellationTokenSource> _pricelistRegionTokenLookup = new ConcurrentDictionary<string, CancellationTokenSource>();
-
         public static IChangeToken CreateChangeToken(string pricelistId)
         {
             if (string.IsNullOrEmpty(pricelistId))
             {
                 throw new ArgumentNullException(nameof(pricelistId));
             }
-            var cancellationTokenSource = _pricelistRegionTokenLookup.GetOrAdd(pricelistId, new CancellationTokenSource());
-            return new CompositeChangeToken(new[] { CreateChangeToken(), new CancellationChangeToken(cancellationTokenSource.Token) });
+            return new CompositeChangeToken(new[] { CreateChangeToken(), CreateChangeTokenForKey(pricelistId) });
         }
 
         public static void ExpirePricelist(string pricelistId)
         {
-            if (_pricelistRegionTokenLookup.TryRemove(pricelistId, out var token))
-            {
-                token.Cancel();
-            }
+            ExpireTokenForKey(pricelistId);
         }
     }
 }
