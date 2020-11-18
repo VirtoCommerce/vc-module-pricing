b75270dd70152bb1f95348d9b48fcc656d901c8e
Andrei Kolchanov <akak1977@gmail.com>
2020-11-18 14:49:35 +0500

    VP-5618: Add ability to switch changes logging into platform operations log thru platform settings

--- src/VirtoCommerce.PricingModule.Data/Handlers/LogChangesChangedEventHandler.cs
+++ src/VirtoCommerce.PricingModule.Data/Handlers/LogChangesChangedEventHandler.cs
@@ -4,6 +4,8 @@ using Hangfire;
 using VirtoCommerce.Platform.Core.ChangeLog;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Events;
+using VirtoCommerce.Platform.Core.Settings;
+using VirtoCommerce.PricingModule.Core;
 using VirtoCommerce.PricingModule.Core.Events;
 
 namespace VirtoCommerce.PricingModule.Data.Handlers
@@ -11,35 +13,48 @@ namespace VirtoCommerce.PricingModule.Data.Handlers
     public class LogChangesChangedEventHandler : IEventHandler<PriceChangedEvent>
     {
         private readonly IChangeLogService _changeLogService;
+        private readonly ILastModifiedDateTime _lastModifiedDateTime;
+        private readonly ISettingsManager _settingsManager;
 
-        public LogChangesChangedEventHandler(IChangeLogService changeLogService)
+        public LogChangesChangedEventHandler(IChangeLogService changeLogService, ILastModifiedDateTime lastModifiedDateTime, ISettingsManager settingsManager)
         {
             _changeLogService = changeLogService;
+            _lastModifiedDateTime = lastModifiedDateTime;
+            _settingsManager = settingsManager;
         }
 
-        public virtual Task Handle(PriceChangedEvent message)
+        public virtual async Task Handle(PriceChangedEvent message)
         {
-            InnerHandle(message);
-            return Task.CompletedTask;
+            await InnerHandle(message);
         }
 
-        protected virtual void InnerHandle<T>(GenericChangedEntryEvent<T> @event) where T : IEntity
+        protected virtual async Task InnerHandle<T>(GenericChangedEntryEvent<T> @event) where T : IEntity
         {
-            var logOperations = @event.ChangedEntries.Select(x => AbstractTypeFactory<OperationLog>.TryCreateInstance().FromChangedEntry(x)).ToArray();
-            //Background task is used here for performance reasons
-            BackgroundJob.Enqueue(() => LogEntityChangesInBackground(logOperations));
+            var logPricingChangesEnabled = await _settingsManager.GetValueAsync(ModuleConstants.Settings.General.LogPricingChanges.Name, (bool)ModuleConstants.Settings.General.LogPricingChanges.DefaultValue);
+
+            if (logPricingChangesEnabled)
+            {
+                var logOperations = @event.ChangedEntries.Select(x => AbstractTypeFactory<OperationLog>.TryCreateInstance().FromChangedEntry(x)).ToArray();
+                //Background task is used here for performance reasons
+                BackgroundJob.Enqueue(() => LogEntityChangesInBackground(logOperations));
+            }
+            else
+            {
+                // Force reset the date of last data modifications, so that it would be reset even if the Inventory.LogInventoryChanges setting is inactive.
+                _lastModifiedDateTime.Reset();
+            }
         }
 
         [DisableConcurrentExecution(10)]
         // "DisableConcurrentExecutionAttribute" prevents to start simultaneous job payloads.
-	// Should have short timeout, because this attribute implemented by following manner: newly started job falls into "processing" state immediately.
+        // Should have short timeout, because this attribute implemented by following manner: newly started job falls into "processing" state immediately.
         // Then it tries to receive job lock during timeout. If the lock received, the job starts payload.
         // When the job is awaiting desired timeout for lock release, it stucks in "processing" anyway. (Therefore, you should not to set long timeouts (like 24*60*60), this will cause a lot of stucked jobs and performance degradation.)
         // Then, if timeout is over and the lock NOT acquired, the job falls into "scheduled" state (this is default fail-retry scenario).
-	// Failed job goes to "Failed" state (by default) after retries exhausted.
-        public void LogEntityChangesInBackground(OperationLog[] operationLogs)
+        // Failed job goes to "Failed" state (by default) after retries exhausted.
+        public async Task LogEntityChangesInBackground(OperationLog[] operationLogs)
         {
-            _changeLogService.SaveChangesAsync(operationLogs).GetAwaiter().GetResult();
+            await _changeLogService.SaveChangesAsync(operationLogs);
         }
     }
 }
