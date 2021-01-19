41498a83b878914c9bb9fceef2cae5d3d5ba6c70
Andrei Kolchanov <akak1977@gmail.com>
2021-01-19 12:44:43 +0500

    VP-6482: Add ProductPriceDates index to speedup search

--- src/VirtoCommerce.PricingModule.Data/Migrations/20210119074026_AddProductPriceDatesIndex.Designer.cs
+++ src/VirtoCommerce.PricingModule.Data/Migrations/20210119074026_AddProductPriceDatesIndex.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VirtoCommerce.PricingModule.Data.Repositories;
 
 namespace VirtoCommerce.PricingModule.Data.Migrations
 {
     [DbContext(typeof(PricingDbContext))]
-    partial class PricingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210119074026_AddProductPriceDatesIndex")]
+    partial class AddProductPriceDatesIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
