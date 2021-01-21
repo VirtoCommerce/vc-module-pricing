fb35442697efee7e6bb92397221b3ea89c7982ae
Andrei Kolchanov <akak1977@users.noreply.github.com>
2021-01-21 14:49:32 +0500

    VP-6482: Add ProductPriceDates index to speedup search (#118)

---
+++ src/VirtoCommerce.PricingModule.Data/Migrations/20210119074026_AddProductPriceDatesIndex.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VirtoCommerce.PricingModule.Data.Migrations
+{
+    public partial class AddProductPriceDatesIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Price_PricelistId",
+                table: "Price");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PriceId",
+                table: "Price");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PricelistProductDates",
+                table: "Price",
+                columns: new[] { "PricelistId", "ProductId", "StartDate", "EndDate" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PricelistProductDates",
+                table: "Price");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Price_PricelistId",
+                table: "Price",
+                column: "PricelistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PriceId",
+                table: "Price",
+                columns: new[] { "ProductId", "PricelistId" });
+        }
+    }
+}
