61e472fe398ff5ece8b79a3ac613085940051007
aihzuli <gabbassova.aizhuldyz@gmail.com>
2021-08-31 14:23:21 +0600

    PT-2259: Update pricing module

--- src/VirtoCommerce.PricingModule.Core/Services/IPricingSearchService.cs
+++ src/VirtoCommerce.PricingModule.Core/Services/IPricingSearchService.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Threading.Tasks;
 using VirtoCommerce.PricingModule.Core.Model.Search;
 
 namespace VirtoCommerce.PricingModule.Core.Services
 {
+    [Obsolete(@"Need to remove after inheriting IPriceSearchService, IPricelistSearchService, and IPricelistAssignmentSearchService from ISearchServiceService.")]
     public interface IPricingSearchService
     {
         Task<PriceSearchResult> SearchPricesAsync(PricesSearchCriteria criteria);
