1006ea973e1656281d0b8a82cc4ed0a095801db4
getmansky <47969278+getmansky@users.noreply.github.com>
2022-02-18 09:23:25 +0300

    PT-6527: Partial search index update support (#164)
    * Partial index update support
    
    * Getting changes for index is fixed
    
    * Version change
    
    * Using release version of the search module Nuget

--- src/VirtoCommerce.PricingModule.Data/Handlers/IndexPricesProductChangedEventHandler.cs
+++ src/VirtoCommerce.PricingModule.Data/Handlers/IndexPricesProductChangedEventHandler.cs
@@ -1,22 +1,28 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Events;
+using VirtoCommerce.Platform.Core.Jobs;
 using VirtoCommerce.Platform.Core.Settings;
 using VirtoCommerce.PricingModule.Core.Events;
+using VirtoCommerce.PricingModule.Data.Search;
 using VirtoCommerce.SearchModule.Core.Model;
 using VirtoCommerce.SearchModule.Data.BackgroundJobs;
+using VirtoCommerce.SearchModule.Data.Services;
 
 namespace VirtoCommerce.PricingModule.Data.Handlers
 {
     public class IndexPricesProductChangedEventHandler : IEventHandler<PriceChangedEvent>
     {
         private readonly ISettingsManager _settingsManager;
+        private readonly IEnumerable<IndexDocumentConfiguration> _configurations;
 
-        public IndexPricesProductChangedEventHandler(ISettingsManager settingsManager)
+        public IndexPricesProductChangedEventHandler(ISettingsManager settingsManager, IEnumerable<IndexDocumentConfiguration> configurations)
         {
             _settingsManager = settingsManager;
+            _configurations = configurations;
         }
 
         public Task Handle(PriceChangedEvent message)
@@ -32,7 +38,8 @@ namespace VirtoCommerce.PricingModule.Data.Handlers
                     .Select(x => new IndexEntry { Id = x.OldEntry.ProductId, EntryState = EntryState.Modified, Type = KnownDocumentTypes.Product })
                     .ToArray();
 
-                IndexingJobs.EnqueueIndexAndDeleteDocuments(indexEntries);
+                IndexingJobs.EnqueueIndexAndDeleteDocuments(indexEntries,
+                    JobPriority.Normal, _configurations.GetBuildersForProvider(typeof(ProductPriceDocumentChangesProvider)).ToList());
             }
 
             return Task.CompletedTask;
