a9f3880812abb26be09d7113275cafdfd4512382
Andrei Kolchanov <akak1977@gmail.com>
2022-03-30 20:48:48 +0500

    fix: Add comments for the NoDetails response group

--- src/VirtoCommerce.PricingModule.Core/Model/PriceListResponseGroup.cs
+++ src/VirtoCommerce.PricingModule.Core/Model/PriceListResponseGroup.cs
@@ -5,6 +5,9 @@ namespace VirtoCommerce.PricingModule.Core.Model
     [Flags]
     public enum PriceListResponseGroup
     {
+        /// <summary>
+        /// Skip details composed into the pricelist (i.e. price list assignments) to speed up pricelists enlisting 
+        /// </summary>
         NoDetails = 1,
         Full = 2
     }
