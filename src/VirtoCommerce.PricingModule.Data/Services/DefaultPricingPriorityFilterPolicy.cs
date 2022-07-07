0b8efb7cdd7e829f553339d21f188a9a34d38248
Getmansky <mge@virtoway.com>
2022-07-07 14:35:14 +0300

    Controll flow nesting reducing

--- src/VirtoCommerce.PricingModule.Data/Services/DefaultPricingPriorityFilterPolicy.cs
+++ src/VirtoCommerce.PricingModule.Data/Services/DefaultPricingPriorityFilterPolicy.cs
@@ -36,37 +36,49 @@ namespace VirtoCommerce.PricingModule.Data.Services
             {
                 foreach (var productPrices in prices.GroupBy(x => x.ProductId))
                 {
-                    var priceTuples = productPrices
-                        .Select(x => new { Price = x, x.Currency, x.MinQuantity, Priority = evalContext.Pricelists.FirstOrDefault(y => y.Id == x.PricelistId)?.Priority })
-                        .Where(x => x.Priority != null);
+                    result.AddRange(SelectBestProductPrices(productPrices, evalContext.Pricelists, certainDate));
+                }
+            }
+            return result;
+        }
 
-                    // Group by Currency and by MinQuantity
-                    foreach (var pricesGroupByCurrency in priceTuples.GroupBy(x => x.Currency))
+        private static List<Price> SelectBestProductPrices(IGrouping<string, Price> productPrices, IEnumerable<Pricelist> pricelists, DateTime certainDate)
+        {
+            var result = new List<Price>();
+
+            var priceTuples = productPrices
+                .Select(x => new
+                {
+                    Price = x, x.Currency, x.MinQuantity, pricelists.FirstOrDefault(y => y.Id == x.PricelistId)?.Priority
+                })
+                .Where(x => x.Priority != null);
+
+            // Group by Currency and by MinQuantity
+            foreach (var pricesGroupByCurrency in priceTuples.GroupBy(x => x.Currency))
+            {
+                var minAcceptablePriority = 0;
+                // take prices with lower MinQuantity first
+                foreach (var pricesGroupByMinQuantity in pricesGroupByCurrency
+                             .GroupBy(x => x.MinQuantity)
+                             .OrderBy(x => x.Key))
+                {
+                    // Take start/end date most close to certainDate, because that price is more specific in time.
+                    // Take minimal price from most prioritized Pricelist.
+                    var groupAcceptablePrice = pricesGroupByMinQuantity
+                        .OrderByDescending(x => x.Priority)
+                        .ThenBy(x => certainDate.Subtract(x.Price.StartDate.GetValueOrDefault(DateTime.MinValue)).TotalSeconds)
+                        .ThenBy(x => x.Price.EndDate.GetValueOrDefault(DateTime.MaxValue).Subtract(certainDate).TotalSeconds)
+                        .ThenBy(x => Math.Min(x.Price.Sale ?? x.Price.List, x.Price.List))
+                        .First();
+
+                    if (groupAcceptablePrice.Priority >= minAcceptablePriority)
                     {
-                        var minAcceptablePriority = 0;
-                        // take prices with lower MinQuantity first
-                        foreach (var pricesGroupByMinQuantity in pricesGroupByCurrency
-                                     .GroupBy(x => x.MinQuantity)
-                                     .OrderBy(x => x.Key))
-                        {
-                            // Take start/end date most close to certainDate, because that price is more specific in time.
-                            // Take minimal price from most prioritized Pricelist.
-                            var groupAcceptablePrice = pricesGroupByMinQuantity
-                                .OrderByDescending(x => x.Priority)
-                                .ThenBy(x => certainDate.Subtract(x.Price.StartDate.GetValueOrDefault(DateTime.MinValue)).TotalSeconds)
-                                .ThenBy(x => x.Price.EndDate.GetValueOrDefault(DateTime.MaxValue).Subtract(certainDate).TotalSeconds)
-                                .ThenBy(x => Math.Min(x.Price.Sale ?? x.Price.List, x.Price.List))
-                                .First();
-
-                            if (groupAcceptablePrice.Priority >= minAcceptablePriority)
-                            {
-                                minAcceptablePriority = groupAcceptablePrice.Priority.Value;
-                                result.Add(groupAcceptablePrice.Price);
-                            }
-                        }
+                        minAcceptablePriority = groupAcceptablePrice.Priority.Value;
+                        result.Add(groupAcceptablePrice.Price);
                     }
                 }
             }
+
             return result;
         }
     }
