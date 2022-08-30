2484c06a8f0cea9191d92d42ca813b4c26d23b57
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2022-08-30 09:32:30 +0200

    PT-8571, PT-8365: remove StorePricesInIndex setting (#179)

--- src/VirtoCommerce.PricingModule.Data/Search/ProductPriceDocumentBuilder.cs
+++ src/VirtoCommerce.PricingModule.Data/Search/ProductPriceDocumentBuilder.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Settings;
-using VirtoCommerce.PricingModule.Core;
 using VirtoCommerce.PricingModule.Core.Model;
 using VirtoCommerce.PricingModule.Core.Services;
 using VirtoCommerce.SearchModule.Core.Model;
@@ -15,12 +14,10 @@ namespace VirtoCommerce.PricingModule.Data.Search
     public class ProductPriceDocumentBuilder : IIndexDocumentBuilder
     {
         private readonly IPricingService _pricingService;
-        private readonly ISettingsManager _settingsManager;
 
         public ProductPriceDocumentBuilder(IPricingService pricingService, ISettingsManager settingsManager)
         {
             _pricingService = pricingService;
-            _settingsManager = settingsManager;
         }
 
         public virtual async Task<IList<IndexDocument>> GetDocumentsAsync(IList<string> documentIds)
@@ -41,10 +38,6 @@ namespace VirtoCommerce.PricingModule.Data.Search
 
             if (prices != null)
             {
-                if (_settingsManager.GetValue(ModuleConstants.Settings.General.StorePricesInIndex.Name, false))
-                {
-                    document.Add(new IndexDocumentField("__prices", prices) { IsRetrievable = false, IsFilterable = false, IsCollection = false });
-                }
                 foreach (var price in prices)
                 {
                     document.Add(new IndexDocumentField($"price_{price.Currency}_{price.PricelistId}".ToLowerInvariant(), price.EffectiveValue) { IsRetrievable = true, IsFilterable = true });
