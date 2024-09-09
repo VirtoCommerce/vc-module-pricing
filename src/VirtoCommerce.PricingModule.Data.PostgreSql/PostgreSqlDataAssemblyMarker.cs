ff05a8510bf57e208f026707c68db8ebc34c17a0
artem-dudarev <ad@virtoway.com>
2024-09-09 16:54:19 +0200

    Fix performance degradation on SQL Server

---
+++ src/VirtoCommerce.PricingModule.Data.PostgreSql/PostgreSqlDataAssemblyMarker.cs
@@ -0,0 +1,5 @@
+namespace VirtoCommerce.PricingModule.Data.PostgreSql;
+
+public class PostgreSqlDataAssemblyMarker
+{
+}
---
+++ src/VirtoCommerce.PricingModule.Data.SqlServer/SqlServerDataAssemblyMarker.cs
@@ -0,0 +1,5 @@
+namespace VirtoCommerce.PricingModule.Data.SqlServer;
+
+public class SqlServerDataAssemblyMarker
+{
+}
