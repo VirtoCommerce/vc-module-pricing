a404a168eb9b6445b1396ca5303d622560a03f54
Basil Kotov <basilkot@gmail.com>
2024-12-02 16:22:44 +0000

    use ui-scroll-drop-down instead of ui-select

--- src/VirtoCommerce.PricingModule.Web/Scripts/selectors/catalog-selector.js
+++ src/VirtoCommerce.PricingModule.Web/Scripts/selectors/catalog-selector.js
@@ -1,6 +1,6 @@
 angular.module('virtoCommerce.pricingModule')
     .controller('virtoCommerce.pricingModule.catalogSelectorController', ['$scope', 'virtoCommerce.catalogModule.catalogs', function ($scope, catalogs) {
-        catalogs.search({ take: 1000 }, function (result) {
-            $scope.catalogs = angular.copy(result.results);
-        });
+        $scope.blade.fetchCatalogs= function (criteria) {
+            return catalogs.search(criteria);
+        }
     }]);
--- src/VirtoCommerce.PricingModule.Web/Scripts/selectors/pricelist-selector.js
+++ src/VirtoCommerce.PricingModule.Web/Scripts/selectors/pricelist-selector.js
@@ -1,6 +1,6 @@
 angular.module('virtoCommerce.pricingModule')
 .controller('virtoCommerce.pricingModule.pricelistSelectorController', ['$scope', 'virtoCommerce.pricingModule.pricelists', function ($scope, pricelists) {
-    pricelists.search({ take: 1000 }, function (result) {
-    	$scope.pricelists = result.results;
-    });
+    $scope.blade.fetchPricelists = function (criteria) {
+        return pricelists.search(criteria);
+    };
 }]);
