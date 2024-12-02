f034e84daa719f2ee51a5fb615fd223a2ca71de1
Basil Kotov <basilkot@gmail.com>
2024-12-02 16:35:49 +0000

    update dependencies

--- src/VirtoCommerce.PricingModule.Web/Scripts/selectors/catalog-selector.js
+++ src/VirtoCommerce.PricingModule.Web/Scripts/selectors/catalog-selector.js
@@ -1,6 +1,6 @@
 angular.module('virtoCommerce.pricingModule')
     .controller('virtoCommerce.pricingModule.catalogSelectorController', ['$scope', 'virtoCommerce.catalogModule.catalogs', function ($scope, catalogs) {
-        $scope.blade.fetchCatalogs= function (criteria) {
+        $scope.blade.fetchCatalogs = function (criteria) {
             return catalogs.search(criteria);
         }
     }]);
